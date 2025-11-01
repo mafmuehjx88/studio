5898a841f86ce9ebda65ea638a4c7f93d0893580
marr ci <marrci448@gmail.com>
2025-11-01 06:27:31 +0000

    Try fixing this error: `Runtime Error: Error: Service account object mus

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -4,9 +4,9 @@ import * as admin from 'firebase-admin';
 // This is a server-only file.
 
 const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 if (!admin.apps.length) {
