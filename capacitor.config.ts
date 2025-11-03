253ef0817460c6209e35629cb17f12a151420520
marr ci <marrci448@gmail.com>
2025-11-03 09:57:54 +0000

    အသူံးပြုမယ် ဘာမှမပြောင်းရင်ရပြီ

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atgamehub.app',
+  appName: 'AT Game HUB',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+  },
+};
+
+export default config;
